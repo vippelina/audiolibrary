6d1ddad1e113d8d31ca970d82ff7857d58fe5c18
vippelina <vibeketengroth@gmail.com>
2018-10-31 13:58:01 +0100

    linting of appfile

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,37 @@
- import express, { Request, Response, NextFunction } from 'express'
-import mongoose from 'mongoose'
-import { MONGODB_URI } from './util/secrets'
-import path from 'path'
-import bodyParser from 'body-parser'
-import indexRoute from './routes';
+ import express, { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
+import { MONGODB_URI } from "./util/secrets";
+import path from "path";
+import bodyParser from "body-parser";
+import indexRoute from "./routes";
 // Create express server
-const app = express()
+const app = express();
 
-//omit deprecation errors
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-//Connect to mongo
+// omit deprecation errors
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+// Connect to mongo
 console.log("monGODB URI", MONGODB_URI);
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true}).then(() => {
-    //  
+    //
 })
 .catch((err: Error) => {
     console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
     process.exit(0);
-})
+});
 
 app.set("port", process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', indexRoute);
+app.use("/", indexRoute);
 
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const isMongoBadInputError = err.name === 'ValidationError' || err.code === 11000
+    const isMongoBadInputError = err.name === "ValidationError" || err.code === 11000;
     const statusCode = isMongoBadInputError ? 400 : 500;
-    res.status(statusCode)
-    const hideErrorMessage = process.env.NODE_ENV === 'production' && statusCode === 500
-    if(!hideErrorMessage) return res.send(err.message)
-    res.send()
-}
+    res.status(statusCode);
+    const hideErrorMessage = process.env.NODE_ENV === "production" && statusCode === 500;
+    if (!hideErrorMessage) return res.send(err.message);
+    res.send();
+};
 app.use(errorHandler);
-export default app  
+export default app;
