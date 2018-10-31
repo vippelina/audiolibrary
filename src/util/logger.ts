5b2e7c3a8a8dbcae7abe348eba583dd4dcbf3480
vippelina <vibeketengroth@gmail.com>
2018-10-31 13:57:21 +0100

    lint config fixes and changes after autolinting

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,12 +1,12 @@
-import winston from 'winston'
+import winston from "winston";
 
  const logger = winston.createLogger({
     transports: [
         new (winston.transports.Console)({
-            level: process.env.NODE_ENV === 'production' ? "error" : "debug"
+            level: process.env.NODE_ENV === "production" ? "error" : "debug"
         }),
         new (winston.transports.File)({filename: "debug.log", level: "debug"})
     ]
-})
+});
 
-export default logger
+export default logger;
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv'
-dotenv.config()
-import logger from './logger'
+import dotenv from "dotenv";
+dotenv.config();
+import logger from "./logger";
 
-export const ENVIRONMENT = process.env.NODE_ENV
-export const MONGODB_URI = ENVIRONMENT !== 'production' 
+export const ENVIRONMENT = process.env.NODE_ENV;
+export const MONGODB_URI = ENVIRONMENT !== "production"
     ? process.env.MONGODB_URI_LOCAL
-    : process.env.MONGODB_URI
+    : process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
     logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
