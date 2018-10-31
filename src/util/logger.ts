86416d728224df24420dc09960c98062a2f9f1d3
vippelina <vibeketengroth@gmail.com>
2018-10-31 13:26:17 +0100

    added logger and secrets file for exposure of env variables

---
+++ src/util/logger.ts
@@ -0,0 +1,12 @@
+import winston from 'winston'
+
+ const logger = winston.createLogger({
+    transports: [
+        new (winston.transports.Console)({
+            level: process.env.NODE_ENV === 'production' ? "error" : "debug"
+        }),
+        new (winston.transports.File)({filename: "debug.log", level: "debug"})
+    ]
+})
+
+export default logger
